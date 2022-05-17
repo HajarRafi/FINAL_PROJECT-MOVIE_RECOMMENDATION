7b9b7610ecea15c003d6e665599b100933a83b57
HajarRafi <hajar.rafibayli@gmail.com>
2022-05-17 08:31:07 +0200

    Python - SQL queries

--- Python&SQL/Queries.sql
+++ Python&SQL/Queries.sql
@@ -17,4 +17,27 @@ from genres;
 
 -- transform some tables for Tableau visualization (too much data..leave only popular ones)
 
-sel
+select movie_id, id as company_id, name as company
+from companies 
+where name in (select name from 
+(select name, count(*) as n_movies
+from companies
+group by name
+having n_movies > 20) sub);
+
+select movie_id, id as company_id, name as company
+from companies 
+where name in (select name from 
+(select name, count(*) as n_movies
+from companies
+group by name
+having n_movies > 20) sub);
+
+
+select movie_id, name as director
+from directors 
+where name in (select name from 
+(select name, count(*) as n_movies
+from directors
+group by name
+having n_movies > 20) sub);
